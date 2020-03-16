b4995bc24558214cf015ee82f2405c356ac0f398
fxf111 <fxf12345@139.com>
2020-03-16 18:19:56 +0800

    update: 首页文案

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    base: '/pixijs',
-    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
+    base: '/pixijs/',
+    title: 'Pixi.js中文网', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: 'Pixi.js中文网', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
         ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
