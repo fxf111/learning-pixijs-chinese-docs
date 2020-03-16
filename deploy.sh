d4fdddeab0e54a3800be5823541b1f0bb596680d
fxf111 <fxf12345@139.com>
2020-03-16 17:25:59 +0800

    config

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:dwanda/dwanda.github.io.git master
+git push -f git@github.com:fxf111/pixijs.github.io.git master:gh-pages
 
 cd -
